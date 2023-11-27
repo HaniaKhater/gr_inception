644fb46b0f60852d677fd0b448294f2cfdac72e6
HaniaKhater <hania.khater2@hotmail.com>
2023-11-27 17:51:02 +0200

    wordpress done

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-service mysql start
+# service mysql start
+
+exec mysqld_safe &
+
+until mysqladmin ping >/dev/null 2>&1; do
+    sleep 1
+done
 
 # Create a MySQL database if it doesn't already exist
 mysql -u root -p"$SQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;"
 
 # Create a MySQL user if it doesn't already exist, identified by the provided password
-mysql -u root -p"$SQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS \`$SQL_USER\`@'localhost' IDENTIFIED BY '$SQL_PWD';"
-
-# Grant all privileges on the specified database to the user, both locally and from any host
-mysql -u root -p"$SQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO \`$SQL_USER\`@'%' IDENTIFIED BY '$SQL_PWD';"
+mysql -u root -p"$SQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'localhost' IDENTIFIED BY '$SQL_PWD';"
+mysql -u root -p"$SQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PWD';"
 
 # Change the password for the root user
 mysql -u root -p"$SQL_ROOT_PASSWORD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_PWD';"
 
 # Flush the MySQL privileges to ensure they are updated
 mysql -u root -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-
-echo "MariaDB initialization completed successfully."
 
 # Shutdown the MySQL server gracefully using mysqladmin
 mysqladmin -u root -p"$SQL_ROOT_PASSWORD" shutdown
